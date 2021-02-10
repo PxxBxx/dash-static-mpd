6bef8ba91e05c547fe218c05853bf68718070d23
PxxBxx <scalplahienne@gmail.com>
2021-02-10 18:11:48 +0100

    Working HLS merging!

--- StaticGeneratorAtomicHLS.class.php
+++ StaticGeneratorAtomicHLS.class.php
@@ -18,25 +18,101 @@ class StaticGeneratorAtomicHLS {
     private function generateStaticHls() {
         // Init container
         $staticManifest = array(
-            'header' => array('#EXTM3U','#EXT-X-VERSION:4','## Created with Brain and Fingers'),
+            'header' => array('#EXTM3U','#EXT-X-VERSION:5','## Created with Brain and Fingers'),
             'extXSessionKey' => array(),
             'audio' => array('# AUDIO groups'),
             'subs' => array('# SUBTITLES groups'),
             'variants' => array('# variants'),
+            'variantsAudio' => array('# variants'),
             'keyframes' => array('# keyframes'),
         );
-        
+
+        // we need Group IDs below...
+        $audioGroup = null;
+        $textGroup = null;
+
         // fetch all m3u8
-        $m3u8s = array();
-        foreach ($this->payload->streams as $type => $uris) {    
+        $m3u8s = array('audio' => array(), 'text' => array(), 'video' => array());
+        foreach ($this->payload->streams as $type => $uris) {
             foreach ($uris as $uri) {
                 $url = $this->payload->origin . $uri;
-                $m3u8s[$type][] = $this->getMasterM3u8($url);
+                $master = $this->getAtomicMaster($url);
+                $m3u8s[$type][] = $master;
+                if (in_array($master->type, array('audio','text')) && is_null(${$master->type."Group"})) {
+                    ${$master->type."Group"} = $master->groupId;
+                }
             }
         }
 
+        // Let's merge and magic
+        // AUDIO
+        foreach ($m3u8s['audio'] as $idx => $audio) {
+            if ($idx === 0) {
+                // First Audio, add a dedicated Variant
+                $variantAudio = $audio->variantLine;
+                if (!is_null($textGroup)) {
+                    // there are subs, must associate to group
+                    $variantAudio .= ',SUBTITLES="' . $textGroup . '"';
+                }
+                $staticManifest['variantsAudio'][] = $variantAudio;
+                $staticManifest['variantsAudio'][] = $audio->uri;
+            }
+            $audioLine = $audio->groupLine . ',URI="' . $audio->uri . '"';
+            if ($idx > 0) {
+                // remove ,DEFAULT=YES for tracks > 0
+                $audioLine = str_replace(',DEFAULT=YES', '', $audioLine);
+            }
+            $staticManifest['audio'][] = $audioLine;
+        }
+        // TEXT
+        foreach ($m3u8s['text'] as $idx => $text) {
+            $textLine = $text->groupLine . ',URI="' . $text->uri . '"';
+            // remove ,DEFAULT=YES
+            $textLine = str_replace(',DEFAULT=YES','',$textLine);
+            // ensure ,AUTOSELECT=NO
+            $textLine = str_replace(',AUTOSELECT=YES',',AUTOSELECT=NO',$textLine);
+
+            $staticManifest['subs'][] = $textLine;
+        }
+
+        // VIDEO
+        foreach ($m3u8s['video'] as $idx => $video) {
+            $variantVideo = $video->variantLine;
+            // Add audio Group
+            if (!is_null($audioGroup)) {
+                $variantVideo .= ',AUDIO="' . $audioGroup . '"';
+            }
+            // Add text Group
+            if (!is_null($textGroup)) {
+                $variantVideo .= ',SUBTITLES="' . $textGroup . '"';
+            }
+
+            // Add Variant
+            $staticManifest['variants'][] = $variantVideo; // Header
+            $staticManifest['variants'][] = $video->uri; // URI
+            // Add Keyframe
+            if (!empty($video->keyframe)) {
+                $staticManifest['keyframes'][] = $video->keyframe;
+            }
+            // Add DRM Key
+            if (!empty($video->key) && !in_array($video->key, $staticManifest['extXSessionKey'])) {
+                $staticManifest['extXSessionKey'][] = $video->key;
+            }
+        }
+
+
+
+/*            //$master = $this->getMasterM3u8($url);
+            // DRM key ?
+            if (!empty($master['extXSessionKey']) && !in_array($master['extXSessionKey'][0], $staticManifest['extXSessionKey'])) {
+                $staticManifest['extXSessionKey'][] = $master['extXSessionKey'][0]; 
+            }
+            $staticManifest['variants'][] = $master['variants'][0] . ',AUDIO="audio-aacl-96"'; // Add reference to AUDIO Group inconditionnaly
+            $staticManifest['variants'][] = $master['variants'][1]; // Add URI stream
+    
+        // TO TRASH :-////////
         // Process audio (group, ...)
-        $audioGroupId = null;
+        /*$audioGroupId = null;
         $hasDefault = false;
         if (isset($m3u8s['audio'])) {
             foreach ($m3u8s['audio'] as $audioM3u8) {
@@ -48,7 +124,7 @@ class StaticGeneratorAtomicHLS {
                 // get group id
                 //if (preg_match("##"))
             }
-        }
+        }*/
 
         // Process subs (group, ...)
 
@@ -60,12 +136,88 @@ class StaticGeneratorAtomicHLS {
     }
 
     /**
+     * Get Atomic master
+     */
+    private function getAtomicMaster($url) {
+        $type = null;
+        $key = null;
+        $groupLine = null;
+        $groupId = null;
+        $variantLine = null;
+        $uri = null;
+        $keyframe = null;
+
+        // Get Base URI to patch URI    s
+        $baseUri = '';
+        if (preg_match("#^https?://[^/]+(/.*\.ism/)[^/]*\.m3u8#", $url, $m)) {
+            $baseUri = $m[1];
+        }
+
+        $lines = file($url, FILE_IGNORE_NEW_LINES);
+        $nbLines = count($lines);
+        for ($i=0; $i<$nbLines; $i++) {
+            $line = $lines[$i];
+            switch(true) {
+                case (preg_match("%^#EXT-X-SESSION-KEY:.*%", $line, $m)):
+                    // DRM KEY
+                    $key = $line;
+                    break;
+                case (preg_match("%^#EXT-X-STREAM-INF:.*CODECS=\"([^\"]*)\"%", $line, $m)):
+                    // Variant and URI
+                    switch (true) {
+                        case (preg_match("#avc1\.#", $m[1])):
+                            $type = 'video';
+                            break;
+                        case (preg_match("#mp4a#", $m[1])):
+                            $type = 'audio';
+                            break;
+                        default:
+                            $type = 'text';
+                            break;
+                    }
+                    $variantLine = $line;
+                    if (isset($lines[$i+1])) {
+                        $uri = $baseUri . $lines[$i+1];
+                    }
+                    break;
+                case (preg_match("%^#EXT-X-I-FRAME-STREAM-INF:.*URI=\"([^\"]+)\".*$%", $line, $m)):
+                    // Keyframe (video only)
+                    $keyframe = str_replace($m[1], $baseUri . $m[1], $line);
+                    break;
+                case (preg_match("%^(#EXT-X-MEDIA:.*,GROUP-ID=\"([^\"]+)\".*),URI=\"[^\"]+\"%", $line, $m)):
+                    // Group (Audio, Text)
+                    $groupLine = $m[1]; // remove ,URI="..."
+                    $groupId = $m[2];
+                    break;
+                case (preg_match("%^(#EXT-X-MEDIA:.*,GROUP-ID=\"([^\"]+)\".*)%", $line, $m)):
+                    // Group (Audio, Text)
+                    $groupLine = $m[1];
+                    $groupId = $m[2];
+                    break;
+            }
+        }
+
+        $toReturn = (object)array(
+            'type' => $type,
+            'key' => $key,
+            'groupLine' => $groupLine,
+            'groupId' => $groupId,
+            'variantLine' => $variantLine,
+            'uri' => $uri,
+            'keyframe' => $keyframe
+        );
+
+        return $toReturn;
+
+    }
+
+    /**
      * Get Master HLS Manifest and patch its resources (media playlist URL) so it can be served statically
      */
     private function getMasterM3u8($url) {
         $lines = file($url, FILE_IGNORE_NEW_LINES);
         $baseUri = '';
-        if (preg_match("#^(.*\.ism/)[^/]*\.m3u8#", $url, $m)) {
+        if (preg_match("#^https?://[^/]+(/.*\.ism/)[^/]*\.m3u8#", $url, $m)) {
             $baseUri = $m[1];
         }
         // Structurate the manifest
@@ -89,33 +241,29 @@ class StaticGeneratorAtomicHLS {
                 break; 
             case (preg_match("%^# AUDIO groups%", $line)):
                 $stepId = 2;
-                $manifest[$steps[$stepId]][] = $lines[++$i];
+                //$manifest[$steps[$stepId]][] = $lines[++$i];
                 break; 
             case (preg_match("%^# SUBTITLES groups%", $line)):
                 $stepId = 3;
-                $manifest[$steps[$stepId]][] = $lines[++$i];
+                //$manifest[$steps[$stepId]][] = $lines[++$i];
                 break; 
             case (preg_match("%^# variants%", $line)):
                 $stepId = 4;
-                $manifest[$steps[$stepId]][] = $lines[++$i];
+                //$manifest[$steps[$stepId]][] = $lines[++$i];
                 break; 
             case (preg_match("%^# keyframes%", $line)):
                 $stepId = 5;
-                $manifest[$steps[$stepId]][] = $lines[++$i];
+                //$manifest[$steps[$stepId]][] = $lines[++$i];
                 break; 
             case (preg_match("%^#EXT-X-.*,URI=\"([^\"]+)\"%", $line, $m)):
                 $newUri = $baseUri . $m[1];
                 $newLine = str_replace($m[1], $newUri, $line); // Patch URI to use absolute original URI
-                if (preg_match("#RESOLUTION=\d+x(\d+),#", $line, $m)) {
-                $manifest[$steps[$stepId]][(string)$m[1]] = $newLine;
-                }
-                else {
                 $manifest[$steps[$stepId]][] = $newLine;
-                }
                 break;
-            case (preg_match("%^#EXT-X-STREAM-INF:.*RESOLUTION=\d+x(\d+),%", $line, $m) && isset($lines[$i+1])):
+            case (preg_match("%^#EXT-X-STREAM-INF:.*,RESOLUTION=%", $line) && isset($lines[$i+1])):
                 $newLine = $baseUri . $lines[$i+1];
-                $manifest[$steps[$stepId]][(string)$m[1]] = $line . "\n" . $newLine;
+                $manifest[$steps[$stepId]][] = $line;
+                $manifest[$steps[$stepId]][] = $newLine;
                 $i++;
                 break;
             default:
@@ -125,8 +273,11 @@ class StaticGeneratorAtomicHLS {
                 break;
             }
         }
+        //  var_dump($manifest); die;
         return $manifest;
+    
     }
+
     private function rebuildMasterM3u8($manifest) {
         $m3u8 = '';
         $m3u8 .= implode("\n", $manifest['header'])."\n";
@@ -134,6 +285,7 @@ class StaticGeneratorAtomicHLS {
         $m3u8 .= implode("\n", $manifest['audio'])."\n"."\n";
         $m3u8 .= implode("\n", $manifest['subs'])."\n"."\n";
         $m3u8 .= implode("\n", $manifest['variants'])."\n"."\n";
+        $m3u8 .= implode("\n", $manifest['variantsAudio'])."\n"."\n";
         $m3u8 .= implode("\n", $manifest['keyframes'])."\n";
         return $m3u8;
     }
--- test-atomic-to-static.php
+++ test-atomic-to-static.php
@@ -31,12 +31,12 @@ $payloadHLSDRM = (object)array(
     'origin' => 'https://usp.minitibone.com',
     'streams' => array(
         'video' => array(
-            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_39366e7f2b94570941b8fdcaacfe708d_drm_224p_hls_v1.ism/Manifest.m3u8',
-            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_a815347490e2518dc601f97cd80f3501_drm_360p_hls_v1.ism/Manifest.m3u8',
-            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_34d25b8e381848be678cad638da8a176_drm_404p_hls_v1.ism/Manifest.m3u8',
-            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_2863760560ad468bc475bab3901b815b_drm_540p_hls_v1.ism/Manifest.m3u8',
-            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_e3e89100b3afde342d2ec45ef7163879_drm_720p_hls_v1.ism/Manifest.m3u8',
-            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_7fcd7011ee63f656521c4438403f7389_drm_1080p_hls_v1.ism/Manifest.m3u8',
+            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_39366e7f2b94570941b8fdcaacfe708d_drm_software_224p_hls_v1.ism/Manifest.m3u8',
+            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_a815347490e2518dc601f97cd80f3501_drm_software_360p_hls_v1.ism/Manifest.m3u8',
+            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_34d25b8e381848be678cad638da8a176_drm_software_404p_hls_v1.ism/Manifest.m3u8',
+            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_2863760560ad468bc475bab3901b815b_drm_software_540p_hls_v1.ism/Manifest.m3u8',
+            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_e3e89100b3afde342d2ec45ef7163879_drm_hardware_720p_hls_v1.ism/Manifest.m3u8',
+            '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_7fcd7011ee63f656521c4438403f7389_drm_hardware_1080p_hls_v1.ism/Manifest.m3u8',
         ),
         'audio' => array(
             '/usp-dev/00ffc4f4c412fc72c02b71cfa222213267a25a30ef30cf44602516fe83b9ecaf_4d82e4dfbdbade4dc3dee7329d051a0d_t1_lv_st1_hls_v1.ism/Manifest.m3u8',
@@ -115,11 +115,11 @@ $payloadDASHDRMSimple = (object)array(
 
 );
 
-//$payload = $payloadHLSClear;
+$payload = $payloadHLSClear;
 //$payload = $payloadHLSDRM;
 //$payload = $payloadDASHClear;
 //$payload = $payloadDASHDRM;
-$payload = $payloadDASHDRMSimple;
+//$payload = $payloadDASHDRMSimple;
 
 $me = new StaticGeneratorAtomic();
 $me->setPayload($payload);
